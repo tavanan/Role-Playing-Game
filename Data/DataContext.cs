7dd015120857b877a9895c291fbe788233c12cb4
tavanan <nadertavana888@gmail.com>
2021-11-23 20:59:29 +0100

    Implementing the DataContext

---
+++ Data/DataContext.cs
@@ -0,0 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using Role_Playing_Game.Models;
+
+namespace Role_Playing_Game.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Character> Characters { get; set; }
+    }
+}
