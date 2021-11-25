340f402a325bb81d49c8afbe98695ad914e71064
tavanan <nadertavana888@gmail.com>
2021-11-25 14:20:04 +0100

    Add IAuthRepository

---
+++ Data/AuthRepository.cs
@@ -0,0 +1,8 @@
+namespace Role_Playing_Game.Data
+{
+    public class AuthRepository : IAuthRepository
+    {
+        
+        
+    }
+}
---
+++ Data/IAuthRepository.cs
@@ -0,0 +1,12 @@
+using System.Threading.Tasks;
+using Role_Playing_Game.Models;
+
+namespace Role_Playing_Game.Data
+{
+    public interface IAuthRepository
+    {
+        Task<ServiceResponse<int>> Register(User user, string password);
+        Task<ServiceResponse<string>> Login(string username, string password);
+        Task<bool> UserExists(string username);
+    }
+}
